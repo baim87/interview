5e71d9b3cd526e2a0ffd3a1f6a8ffa14461dc537
Balya Malkanul Karim <balya.karim@gmail.com>
2023-06-14 08:54:21 +0700

    Update warranty.php

--- warranty.php
+++ warranty.php
@@ -21,7 +21,7 @@
                      </fieldset>
                 </form>
                 <div>
-                    <button class="formbold-btn" onclick="clickHandler();">Submit</button>
+                    <button class="formbold-btn">Submit</button>
                 </div>
                 <p id="hasil"></p>
             </div>
@@ -29,27 +29,5 @@
  
     </section>
 </main>
- 
-<script>
-
-
-const clickHandler = async () => {
-    const btnWarty = document.querySelector('.formbold-btn');
-    let warapam = document.form_main.warranti.value;
-    let warapama = warapam;
-    const url = 'https://markautodetailing.com/mos/wp-json/mark-order-system/v1/GetUserMaintenance?user_code=' + warapama;
-        fetch(url, {method:'GET',
-            headers: { Authorization : 'Basic bW9zOm9yZGVyX3N5c3RlbV9hY2Nlc3M='},
-        })
-    // .then(response => response.json())
-    // .then(json => console.log(json));
-    .then(response => response.json())
-          .then(response => document.getElementById("hasil").innerHTML =
-            "Hi Mr/Mrs: " + (response.payments.CustName))
-          .catch(error => console.log('error', error));
-
-};
-btnWarty.addEventListener('click', clickHandler);
-</script>
  
     <?php include "includes/footer.php" ?>
